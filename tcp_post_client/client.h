2935b337065b41fb2a42509512f605f7a8293e2b
Patrik Yousef <patrikyousef@example.com>
2025-10-30 16:08:23 +0100

    chore: initiera projekt med Makefile och headerfiler

--- tcp_post_client/client.h
+++ tcp_post_client/client.h
@@ -3,10 +3,10 @@
 
 #include <stddef.h>
 
-// Typ för funktionspekare som hanterar inkommande data
+
 typedef void (*response_handler_t)(const char *data);
 
-// Funktioner som exponeras
+
 int connect_to_server(const char *hostname, int port);
 int send_post_request(int sockfd, const char *hostname, const char *json_body);
 void receive_response(int sockfd, response_handler_t handler);
--- tcp_post_client/handler.c
+++ tcp_post_client/handler.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 
-// Skriver ut allt direkt
+
 void print_handler(const char *data) {
     printf("%s", data);
 }
 
-// Räknar hur många tecken som mottagits
+
 void count_handler(const char *data) {
     static int total = 0;
     total += strlen(data);
